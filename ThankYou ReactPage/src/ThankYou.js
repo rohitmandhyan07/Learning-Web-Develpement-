aeb50b52f7b409e836bea147044cb9167dd9a59a
RMD <rmandhyan54@gmail.com>
2023-06-06 21:07:16 +0530

    Initial Commit

---
+++ ThankYou ReactPage/src/ThankYou.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ThankYou = () => {
+  return (
+    <div>
+      <h1>Thank You!</h1>
+      <p>Thank you for your support and feedback. We appreciate your time.</p>
+      <p>Please feel free to reach out to us if you have any further questions.</p>
+    </div>
+  );
+};
+
+export default ThankYou;
